3d53e6f2904158524c7ae2208b76e5eb5708f760
thomasrileydev <52782404+thomasrileydev@users.noreply.github.com>
2021-11-03 15:09:46 +0000

    Add project files.

---
+++ cypher/BadCypher.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+namespace cypher
+{
+     static class BadCypher
+    {
+      public  static string Encrypt(string input)
+        {
+            List < char > charlist = new List<char>();
+            List<byte> asciilist = new List<byte>();
+            string outputstring = "";
+            foreach (char character in input)
+            {
+                charlist.Add(character);
+            }
+            charlist.Reverse();
+            char firstchar = charlist[0];
+            charlist.Add(firstchar);
+            charlist.RemoveAt(0);
+            for (int index = 0; index < charlist.Count ; index++)
+            {
+                if (charlist[index] == 'z')
+                {
+                    asciilist.Add(97);
+                }
+                else if (charlist[index] == 'Z')
+                {
+                    asciilist.Add(65);
+                }
+                else if (charlist[index]==' ')
+                {
+                    asciilist.Add(32);
+                }
+                else
+                {
+                 asciilist.Add(Convert.ToByte(charlist[index]));
+                    asciilist[index] += Convert.ToByte(1);
+                }
+            }
+            foreach (byte ascii in asciilist)
+            {
+                 outputstring +=Convert.ToChar(ascii); 
+            }
+            return outputstring;
+        }
+        static string Decrypt(string input)
+        {
+            List<char> charlist = new List<char>();
+            foreach (char character in input)
+            {
+                charlist.Add(character);
+            }
+            charlist.Reverse();
+            charlist.Insert(0,charlist[charlist.Count]);
+            charlist.RemoveAt(charlist.Count);
+            return "Error";
+        }
+    }
+}
---
+++ cypher/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+namespace cypher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new CypherProgram();
+        }
+    }
+    class CypherProgram
+    {
+        public CypherProgram()
+        {
+            testsuite();
+        }
+        void testsuite()
+        {
+            {
+                Console.WriteLine(BadCypher.Encrypt("hello"));
+                Debug.Assert(BadCypher.Encrypt("hello")=="mmfip");  
+            }
+        }
+    }
+  
+}
