efe3e34fbccc98c99070ff7da9e6add61ad496b8
thomasrileydev <52782404+thomasrileydev@users.noreply.github.com>
2021-11-05 13:37:36 +0000

    Added More Tests - Works Fine, Final Commit

--- cypher/Program.cs
+++ cypher/Program.cs
@@ -14,18 +14,35 @@ namespace cypher
         public CypherProgram()
         {
             testsuite();
-       }
+        }
         void testsuite()
         {
             {
                 Console.WriteLine(BadCypher.Encrypt("hello"));
-                Debug.Assert(BadCypher.Encrypt("hello")=="mmfip"); 
+                Debug.Assert(BadCypher.Encrypt("hello") == "mmfip");
             }
             {
                 Console.WriteLine(BadCypher.Decrypt("mmfip"));
                 Debug.Assert(BadCypher.Decrypt("mmfip") == "hello");
             }
+            {
+                Console.WriteLine(BadCypher.Encrypt("hi everyone"));
+                Debug.Assert(BadCypher.Encrypt("hi everyone") == "opzsfwf jif");
+            }
+            {
+                Console.WriteLine(BadCypher.Decrypt("opzsfwf jif"));
+                Debug.Assert(BadCypher.Decrypt("opzsfwf jif") == "hi everyone");
+            }
+            {
+                Console.WriteLine(BadCypher.Encrypt("zebras"));
+                Debug.Assert(BadCypher.Encrypt("zebras") == "bscfat");
+            }
+            {
+                Console.WriteLine(BadCypher.Decrypt("bscfat"));
+                Debug.Assert(BadCypher.Decrypt("bscfat") == "zebras");
+            }
+
         }
+
     }
-  
 }
