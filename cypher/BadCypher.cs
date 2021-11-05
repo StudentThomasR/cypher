356b6f4b127e2dd9470b5ef02ab7c59d5d87b809
thomasrileydev <52782404+thomasrileydev@users.noreply.github.com>
2021-11-05 13:10:07 +0000

    Created Decrypt Function

--- cypher/BadCypher.cs
+++ cypher/BadCypher.cs
@@ -43,6 +43,45 @@ namespace cypher
             }
             return outputstring;
         }
-        
+        public static string Decrypt(string input)
+        {
+            List<char> charlist = new List<char>();
+            List<byte> asciilist = new List<byte>();
+            string outputstring = "";
+            foreach (char character in input)
+            {
+                charlist.Add(character);
+            }
+            char firstchar = charlist[0];
+            charlist.Add(firstchar);
+            charlist.RemoveAt(0);
+            charlist.Reverse();
+            for (int index = 0; index < charlist.Count; index++)
+            {
+                if (charlist[index] == 'a')
+                {
+                    asciilist.Add(122);
+                }
+                else if (charlist[index] == 'A')
+                {
+                    asciilist.Add(90);
+                }
+                else if (charlist[index] == ' ')
+                {
+                    asciilist.Add(32);
+                }
+                else
+                {
+                    asciilist.Add(Convert.ToByte(charlist[index]));
+                    asciilist[index] -= Convert.ToByte(1);
+                }
+            }
+            foreach (byte ascii in asciilist)
+            {
+                outputstring += Convert.ToChar(ascii);
+            }
+            return outputstring;
+        }
+
     }
 }
--- cypher/Program.cs
+++ cypher/Program.cs
@@ -14,7 +14,7 @@ namespace cypher
         public CypherProgram()
         {
             testsuite();
-        }
+       }
         void testsuite()
         {
             {
